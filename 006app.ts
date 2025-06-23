044f04484ed08d79b6a17c898533e234d51055eb
Ahtisham Ali <ahtishamrajput819@gmail.com>
2025-06-23 12:03:18 -0700

    object

---
+++ 006app.ts
@@ -0,0 +1,24 @@
+                    //  OBJECTS
+                    //type alies 
+let student = {
+    name: 'Ahtisham ali',
+    experience: "10 years" 
+}
+
+console.log(student.name);
+console.log(student["experience"]);
+
+                //    Type define or declearing
+
+let teacher :{
+    name: string
+    experience: string
+}
+
+ teacher ={
+    name: "saleem",
+    experience: "25 years"
+}
+console.log(teacher["name"]);
+console.log(teacher.experience);
+
